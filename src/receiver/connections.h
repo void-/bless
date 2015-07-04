f3638dba5aa67727d2fe6d7a4aad25256b1a0ec7
void- <mr.alex.guy@gmail.com>
2015-07-04 10:39:27 -0700

    recv: Added extra key parameters to connection functions.

--- src/receiver/connections.h
+++ src/receiver/connections.h
@@ -1,6 +1,11 @@
 #ifndef CONNECTIONS_H
 #define CONNECTIONS_H
 
-int tcpBootstrap(char const * host, unsigned short port, struct sessionKeys *);
-int udpBootstrap(char const * host, unsigned short port, int *socket);
+int tcpBootstrap(char const *host, unsigned short port,
+  struct serverAuthKeys const *, struct sessionKeys *);
+
+int udpBootstrap(char const *host, unsigned short port,
+  struct sessionKeys const *, int *socket);
+
+int listenLoop(int socket, struct sessionKeys const *, struct messageKeys *);
 #endif //CONNECTIONS_H
