69ab8e4a10a51b4a0d9190112dcb3a21aa47ca82
void- <mr.alex.guy@gmail.com>
2015-07-04 10:25:13 -0700

    recv: Added public key declarations.

--- src/receiver/auth.h
+++ src/receiver/auth.h
@@ -4,7 +4,22 @@
 /**
  * The length in bytes of symmetric session keys.
  */
-const unsigned int KEY_BYTES = 16u;
+const unsigned SYM_KEY_BYTES = 16u;
+
+/**
+ * The length in bytes of the public keys used for authentication.
+ */
+const unsigned PUB_KEY_BYTES = 32u;
+
+/**
+ * Contains long-standing authentication keys for the Server and the Receiver.
+ *
+ * Public key of the Server.
+ * Private key of the Receiver.
+ */
+struct serverAuthKeys
+{
+};
 
 /**
  * The symmetric keys derived from the Diffie Hellman exchange between the
@@ -15,4 +30,5 @@ struct sessionKeys
   unsigned char keyEnc[KEY_BYTES];
   unsigned char keyMac[KEY_BYTES];
 };
+
 #endif //AUTH_H
