af6d3a51fc13fd722dd17bec45dedb7f395b1807
void- <mr.alex.guy@gmail.com>
2015-07-20 21:47:40 -0700

    recv: Added Channel class for message channel.

--- src/receiver/connections.h
+++ src/receiver/connections.h
@@ -10,4 +10,29 @@ int udpBootstrap(char const *host, unsigned short port,
   struct sessionKeys const *, int *socket);
 
 int listenLoop(int socket, struct sessionKeys const *, struct messageKeys *);
+
+namespace Bless
+{
+
+  /**
+   * @brief holds connection information about the message channel.
+   *
+   * This is the secure message channel from the Server to the Receiver
+   * implemented as a DTLS connection.
+   *
+   * Packets will arrive when a message is sent from the Sender; these are
+   * unacked.
+   *
+   * When the Receiver shuts down, the DTLS connection will uncleanly be
+   * shutdown, i.e. no finalizing packets will be sent by the Receiver.
+   */
+  class Channel
+  {
+    public:
+      Channel();
+
+    protected:
+  };
+
+}
 #endif //CONNECTIONS_H
