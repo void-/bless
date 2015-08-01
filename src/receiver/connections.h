eae239cb0241db59cb78c4f263a502c4c701e821
void- <mr.alex.guy@gmail.com>
2015-07-31 21:30:07 -0700

    recv: recvCallback takes const pointer.

--- src/receiver/connections.h
+++ src/receiver/connections.h
@@ -46,7 +46,8 @@ namespace Bless
   class Channel
   {
     public:
-      typedef std::function<int (unsigned char *, std::size_t)> recvCallback;
+      typedef std::function<int (unsigned char const * const, std::size_t)>
+        recvCallback;
 
       Channel();
       ~Channel();
