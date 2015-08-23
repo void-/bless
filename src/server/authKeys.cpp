48d7eac511b556e76421234c87bf2d3db637c229
void- <mr.alex.guy@gmail.com>
2015-08-22 20:22:53 -0700

    serv: Outlined classes for authentication keys.

---
+++ src/server/authKeys.cpp
@@ -0,0 +1,12 @@
+#include "authKeys.h"
+
+namespace Bless
+{
+  int CounterpartyKey::init(KeyStore &store)
+  {
+  }
+
+  int ServerKey::init(KeyStore &store)
+  {
+  }
+}
--- src/server/authKeys.h
+++ src/server/authKeys.h
@@ -3,3 +3,71 @@
  * @brief manage the authentication keys of the Receiver and Sender for the
  *   Server.
  */
+
+#ifndef AUTHKEYS_H
+#define AUTHKEYS_H
+
+#include <botan/x509cert.h>
+#include <botan/pk_keys.h>
+
+#include "persistentStore.h"
+
+namespace Bless
+{
+  /**
+   * @class ConnectionKey
+   * @brief abstract class for TLS authentication key stored in persistent
+   *   memory.
+   *
+   * Key types
+   * - Sender's cert to connect to the Server(many)
+   * - Server's cert+key to connect to the Sender(one)
+   * - Server's cert+key to connect to the Receiver(one)
+   * - Receiver's cert to connect to the Server(one)
+   *
+   * @var Botan::X509_Certificate *ConnectionKey::cert
+   * @brief certificate for the party this ConnectionKey represents
+   */
+  class ConnectionKey
+  {
+    public:
+      ConnectionKey();
+      virtual ~ConnectionKey();
+      virtual int init(KeyStore &store) = 0;
+    protected:
+      Botan::X509_Certificate const *cert;
+  };
+
+  /**
+   * @class CounterpartyKey
+   * @brief holds the certificate of either the Sender or Receiver.
+   */
+  class CounterpartyKey : ConnectionKey
+  {
+    public:
+      int init(KeyStore &store) override;
+  };
+
+  /**
+   * @class ServerKey
+   * @brief stores both the certificate and corresponding private key of the
+   *   Server.
+   *
+   * This is used for connections from both the Server to Sender and Server to
+   * Receiver.
+   *
+   * @var Botan::Private_Key *receiverPrivKey
+   * @brief private key for the Server.
+   */
+  class ServerKey : ConnectionKey
+  {
+    public:
+      ~ServerKey() override;
+      int init(KeyStore &store) override;
+
+    private:
+      Botan::Private_Key *receiverPrivKey;
+  };
+}
+
+#endif //AUTHKEYS_H
